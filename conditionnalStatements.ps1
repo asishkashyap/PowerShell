40f9c9db52e53305368d1050462e07441242dadb
Asish Kashyap <129969015+asishkashyap@users.noreply.github.com>
2025-12-03 21:57:48 +0530

    Add files via upload

--- conditionnalStatements.ps1
+++ conditionnalStatements.ps1
@@ -14,8 +14,9 @@ $chanduram = 105
 
 if ($chanduram -eq 100) {
     Write-Host "number is equal"
-}else {
-    Write-Host "dsfgr"
+}
+else {
+    Write-Host "Enter valid number"
 }
 
 
@@ -25,23 +26,92 @@ $fruit = "Guawa"
 
 if ($fruit -eq "Mango") {
     Write-Host " Color is Yellow"
-}elseif($fruit -eq "Apple") {
-     Write-Host " Color is red"
-}elseif($fruit -eq "Kiwi") {
-     Write-Host " Color is Brown"
-}else{
+}
+elseif ($fruit -eq "Apple") {
+    Write-Host " Color is red"
+}
+elseif ($fruit -eq "Kiwi") {
+    Write-Host " Color is Brown"
+}
+else {
     Write-Host "Fruit name is invalid"
 }
 
 
 #DevOps exmaple
+# If condition passes then only action will work
+$status = "dsfvdfvd"
 
-$status = "failed"
+if ($status -eq "success") {
+    Write-Host "Deployment passed"
 
-if ($status -ne "success") {
-    Write-Host "Deployment failed"
-    throw "status is not success, refer the pipeline logs"  # throw is used to end the script and print the custom error message
 }
-else{
-    Write-Host "Deployment is successful"
+elseif ($status -eq "failed") {
+    Write-Host "Deployment is successful1"
+    Write-Host "Deployment is successful2"
+    return  # this command used to break the script and continue the flow   
+}
+else {
+    Write-Host "No status found"
+}
+
+# Write-Host --> It prints the exact message in logs, also used to debug the code
+# Write-Output --> We can use the output of this in other command like pipeline
+
+
+# Switch --> It is used to define multiple no. conditions 
+
+
+$fruit = "Guawa"
+
+if ($fruit -eq "Mango") {
+    Write-Host " Color is Yellow"
+}
+elseif ($fruit -eq "Apple") {
+    Write-Host " Color is red"
+}
+elseif ($fruit -eq "Kiwi") {
+    Write-Host " Color is Brown"
+}
+else {
+    Write-Host "Fruit name is invalid"
+}
+
+
+$fruit = ""
+switch ($fruit) {
+    "Mango" { Write-Host " Color is Yellow" }
+    "Apple" { Write-Host " Color is Red" }
+    "Guawa" { Write-Host " Color is Green" }
+    "Kiwi" { Write-Host " Color is Brown" }
+    Default { Write-Host "Fruit name is invalid" }
+}
+
+
+switch ($number) {
+    { $_ -eq 18 } { Write-Host " Person is adult" }
+    { $_ -lt 18 } { Write-Host " Person is Teenager" }
+    Default { Write-Host "No age provided" }
+}
+
+$numbers = @(12, 19, 20, 30, 80)
+foreach ($i in $numbers) {
+    if ($i -gt 18) {
+        Write-Output " Person is adult: Age is $i" | Add-Content -Path ""
+    }
+    elseif ($i -eq 80){
+          Write-Host "Person is an Senior Citizen"
+    }
+    else {
+        Write-Host "Person is Teenager, Age is $i"
+    }
+}
+
+$numbers = @(12, 19, 20, 30, 80)
+foreach ($i in $numbers) {
+    switch ($number) {
+    { $_ -eq 18 } { Write-Host " Person is adult" }
+    { $_ -lt 18 } { Write-Host " Person is Teenager" }
+    Default { Write-Host "No age provided" }
+}
 }
